dfcbff8d6c7fc13f9671659d5322ecde4f55809d
SpacePirateWZ <spacepiratewz@gmail.com>
2023-07-09 21:28:15 +0800

    Added function for player to input R/P/S and also function to play the game for one round

--- RPS.js
+++ RPS.js
@@ -1,9 +1,44 @@
 
+let computerSelection = getComputerChoice()
+
 function getComputerChoice() {
     const values = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * values.length)
     return values[randomIndex];
 }
 
-console.log(getComputerChoice())
+
+let playerSelection = prompt('Rock, Paper, or Scissors?','Rock')
+
+playerSelection = getPlayerSelection(playerSelection);
+
+computerSelection = computerSelection.toLowerCase()
+
+function getPlayerSelection(playerSelection) {
+    const normalizedSelection = playerSelection.toLowerCase(); // Convert to lowercase
+    
+    if (normalizedSelection === 'rock' || normalizedSelection === 'paper' || normalizedSelection === 'scissors') {
+      return normalizedSelection;
+    } else {
+      throw new Error('Invalid selection. Please choose Rock, Paper, or Scissors.');
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection))
 
